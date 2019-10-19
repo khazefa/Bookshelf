fd973d1730b4ddc8c9967fde6460976aa5c595d0
Sigit Prayitno <cybergitt@gmail.com>
2019-10-19 14:28:22 +0700

    Update App.java
    -Add logic, run command by user input
    -I forgot to change parameter in shelf.setMax_size(5) to shelf.setMax_size(slot_quota)

--- src/main/java/id/cybergitt/App.java
+++ src/main/java/id/cybergitt/App.java
@@ -3,15 +3,17 @@ package id.cybergitt;
 import id.cybergitt.bookshelf.controllers.BookShelf;
 import id.cybergitt.bookshelf.helpers.DigiHelper;
 import id.cybergitt.bookshelf.models.Book;
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Hello world!
+ * Bookshelf
  */
 public class App {
     static BookShelf shelf = new BookShelf();
@@ -39,8 +41,23 @@ public class App {
             }
         } else { // if args doesnt have parameters
             showMenu();
-            int max_size = 5;
-            run_hashmap_slot(max_size);
+            BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+            String inputs = "";
+            try {
+                while (!inputs.equalsIgnoreCase("exit")) {
+                    System.out.print("Enter your command:");
+                    inputs = in.readLine();
+                
+                    if(inputs.isEmpty()){
+                        System.out.println("System exit..");
+                        System.exit(0);
+                    }else{
+                        runByInput(inputs);
+                    }
+                }
+            } catch (IOException e) {
+                System.err.println(e.getMessage());
+            }
         }
     }
     
@@ -56,7 +73,6 @@ public class App {
         System.out.format(leftAlignFormat, "   titles-by-author(author)", "titles-by-author JKRowling");
         System.out.format(leftAlignFormat, "   help", "");
         System.out.format(leftAlignFormat, "   exit", "");
-        System.out.println("Enter your command:  ");
     }
     
     public static void runByFile(String command){
@@ -66,7 +82,7 @@ public class App {
         
         if(cmd.get(0).toString().equalsIgnoreCase("create_book_shelf")){
             int slot_quota = Integer.parseInt(cmd.get(1).toString());
-            shelf.setMax_size(5);
+            shelf.setMax_size(slot_quota);
             System.out.println("Created a book shelf with " +shelf.totalShelves()+ " slots");
         }
         else if(cmd.get(0).toString().equalsIgnoreCase("put")){
@@ -110,11 +126,56 @@ public class App {
         }
     }
     
-    private static void runByInput(){
-        //to do code here
+    public static void runByInput(String command){
+        DigiHelper helper = new DigiHelper();
+        List cmd = new ArrayList();
+        cmd = helper.commandSplitter(command);
+        
+        if(cmd.get(0).toString().equalsIgnoreCase("create_book_shelf")){
+            int slot_quota = Integer.parseInt(cmd.get(1).toString());
+            shelf.setMax_size(slot_quota);
+            System.out.println("Created a book shelf with " +shelf.totalShelves()+ " slots");
+        }
+        else if(cmd.get(0).toString().equalsIgnoreCase("put")){
+            if (shelf.shelvesSize() <= shelf.getMax_size()){
+                int get_slot = shelf.findEmptySlot();
+                if (get_slot > 0){
+                    shelf.updateSlot(get_slot, new Book(cmd.get(1).toString(), cmd.get(2).toString()));
+                } else {
+                    shelf.addSlot(new Book(cmd.get(1).toString(), cmd.get(2).toString()));
+                }
+            } else {
+                System.out.println("Book shelf is full");
+            }
+        }
+        else if(cmd.get(0).toString().equalsIgnoreCase("remove")) {
+            int pos = Integer.parseInt(cmd.get(1).toString());
+            shelf.removeSlot(pos);
+        }
+        else if(cmd.get(0).toString().equalsIgnoreCase("list")) {
+            shelf.getList();
+        }
+        else if(cmd.get(0).toString().equalsIgnoreCase("find-by-author")) {
+            shelf.findByAuthor(cmd.get(1).toString());
+        }
+        else if(cmd.get(0).toString().equalsIgnoreCase("find-by-title")) {
+            shelf.findByTitle(cmd.get(1).toString());
+        }
+        else if(cmd.get(0).toString().equalsIgnoreCase("titles-by-author")) {
+            shelf.titleByAuthor(cmd.get(1).toString());
+        }
+        else if(cmd.get(0).toString().equalsIgnoreCase("help")) {
+            showMenu();
+        }
+        else if(cmd.get(0).toString().equalsIgnoreCase("exit")) {
+            System.exit(0);
+        }
+        else{
+            System.out.println("Command not found!");
+        }
     }
     
-    private static void run_hashmap_slot(int max_size){
+    private static void run_hashmap_slot(int max_size){ // test case
         DigiHelper helper = new DigiHelper();
         BookShelf shelf = new BookShelf(max_size);
         System.out.println("Created a book shelf with " +shelf.totalShelves()+ " slots");
